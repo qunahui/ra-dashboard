17303bb4c72a34c58e5a6a763823558540a2c051
Quang Huy <quanhui812@gmail.com>
2021-06-19 15:26:00 +0700

    fix env

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack')
 
 const paths = {
   DIST: path.resolve(__dirname, '../dist'),
@@ -145,6 +146,15 @@ module.exports = (env) => {
       disableHostCheck: true //for ngrok
     },
     plugins: [
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      }),
       new HtmlWebpackPlugin({
         template: path.join(paths.PUB, 'index.html'),
       }),
