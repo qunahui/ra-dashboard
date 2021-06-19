31b4103327cfe8145d24dcf3b3a1291c839fc27c
Quang Huy <quanhui812@gmail.com>
2021-06-19 15:30:36 +0700

    fix webpack config

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -147,13 +147,13 @@ module.exports = (env) => {
     },
     plugins: [
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+        'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+        'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+        'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+        'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
+        'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
       }),
       new HtmlWebpackPlugin({
         template: path.join(paths.PUB, 'index.html'),
